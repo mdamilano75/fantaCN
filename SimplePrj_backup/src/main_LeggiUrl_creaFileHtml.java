64493c8cf3a9f54d25dceddaf0534d96747011e6
Marco Damilano <mdamilano@siapcn.it>
2024-10-14 08:53:29 +0200

    aggiunto CEN_2

--- SimplePrj_backup/src/main_LeggiUrl_creaFileHtml.java
+++ SimplePrj_backup/src/main_LeggiUrl_creaFileHtml.java
@@ -63,5 +63,23 @@ public class main_LeggiUrl_creaFileHtml {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		url = FC_Varie.url_chiPrenderepref2+FC_Varie.url_chiPrendereCen2;
+		try {
+			Document doc = Jsoup.connect(url).get();
+
+			// Stampa il titolo della pagina
+			System.out.println("Title: " + doc.title());
+
+			// Stampa l'intero contenuto HTML
+//			System.out.println(doc.html());
+			
+			String filePath		= FC_Varie.pathFileGenerati+"/"+FC_Varie.nomeFile_chiPrendereCen2;
+			FileWriter writer = new FileWriter(filePath);
+	        writer.write(doc.html());
+	        writer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
